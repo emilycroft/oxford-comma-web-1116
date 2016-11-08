28a4dc067814a4dedb2aebf6e7c3a45e686b6339
Emily Croft <emilyhcroft@gmail.com>
2016-11-08 13:43:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  result = ""
 
+  if array.length == 1
+    result = array.join
+  elsif array.length == 2
+    array.insert(-2, "and")
+    result = array.join(" ")
+  elsif array.length >= 3
+    result = "#{array[0, (array.length - 1)].join(", ")}, and #{array.last}"
+  end
+
+  return result
 end
